1dc2b5bc5f15c84628300db840b61319445eabe0
Juan Flores-Moroco <ajaflorez.upc@gmail.com>
2020-06-04 12:51:03 -0500

    using WebElement, Select, WebDriverException, and other

---
+++ src/main/java/pe/edu/upc/selenium/Exceptions.java
@@ -0,0 +1,46 @@
+package pe.edu.upc.selenium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Exceptions {
+
+    static WebDriver webDriver;
+    public static void main(String[] args) {
+
+        // Variables
+        String urlPage = "https://www.google.com.pe/";
+        // Establece el chrome driver
+        WebDriver webDriver;
+        String pathDriver = System.getProperty("user.dir") + "\\driver\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", pathDriver);
+        webDriver = new ChromeDriver();
+
+        // Carga la pagina
+        webDriver.get(urlPage);
+
+        // --Maximiza la ventana
+        webDriver.manage().window().maximize();
+
+        try {
+            WebElement inpTexto = webDriver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input"));
+            inpTexto.sendKeys("Cucumber");
+
+            webDriver.findElement(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[3]/center/input[1]")).click();
+
+            String texto = webDriver.findElement(By.xpath("//*[@id=\"rso\"]/div[5]/div/div[2]/div/span")).getText();
+            System.out.println(texto);
+
+        } catch ( NoSuchElementException ne) {
+            System.err.println("ERROR NE: No hay el elemento que estas buscando\n" + ne.getMessage());
+        }
+        catch( WebDriverException we) {
+            System.err.println("ERROR WE: " + we.getMessage());
+        } catch ( Exception e ) {
+            System.err.println("ERROR: " + e.getMessage());
+        } finally {
+            //webDriver.close();
+        }
+
+    }
+}
---
+++ src/main/java/pe/edu/upc/selenium/WebElements.java
@@ -0,0 +1,64 @@
+package pe.edu.upc.selenium;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class WebElements {
+
+    static WebDriver webDriver;
+    public static void main(String[] args) {
+
+        // Variables
+        String urlPage = "http://localhost:8081/onewebs/";
+        // Establece el chrome driver
+        WebDriver webDriver;
+        String pathDriver = System.getProperty("user.dir") + "\\driver\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", pathDriver);
+        webDriver = new ChromeDriver();
+
+        // Carga la pagina
+        webDriver.get(urlPage);
+
+        // --Maximiza la ventana
+        webDriver.manage().window().maximize();
+
+        try {
+            WebElement linkPolice = webDriver.findElement(By.id("link-police"));
+            WebElement linkDetainee = webDriver.findElement(By.name("link-detainee"));
+            WebElement linkMulct = webDriver.findElement(By.xpath("//body//header//nav//div//ul//li[3]//a"));
+
+            linkMulct.click();
+
+            /*WebElement btnNuevoMulct = webDriver.findElement(By.id("btn-nuevo-mulct"));
+            btnNuevoMulct.click();*/
+
+            webDriver.get("http://localhost:8081/onewebs/mulcts/new");
+
+            // Select Detenido
+           Select sltDetainee = new Select( webDriver.findElement(By.id("detenido") ));
+            // Agregando datos al formulario
+            sltDetainee.selectByValue("8");
+            webDriver.findElement(By.id("fechaDetencion")).sendKeys("20/08/2003");
+            webDriver.findElement(By.id("direccion")).sendKeys("Av. Foucet 1458");
+            new Select(webDriver.findElement(By.id("infraccion"))).selectByIndex(1);
+            webDriver.findElement(By.id("monto")).clear();
+            webDriver.findElement(By.id("monto")).sendKeys("150");
+
+            //webDriver.findElement(By.xpath("/html/body/main/section/div/div[2]/form/button")).click();
+            webDriver.findElement(By.xpath("/html/body/main/section/div/div[2]/form/button")).submit();
+
+
+        } catch ( NoSuchElementException ne) {
+            System.err.println("ERROR NE: No hay el elemento que estas buscando\n" + ne.getMessage());
+        }
+        catch( WebDriverException we) {
+            System.err.println("ERROR WE: " + we.getMessage());
+        } catch ( Exception e ) {
+            System.err.println("ERROR: " + e.getMessage());
+        } finally {
+            //webDriver.close();
+        }
+
+    }
+}
